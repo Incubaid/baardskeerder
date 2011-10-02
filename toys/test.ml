b806b0d6af7ec6d6be4c59c08244bc133b2b8fa1
Romain Slootmaekers <rslootma@b-virtual.org>
2011-10-02 17:08:21 +0200

    some changes for blog post

--- toys/test.ml
+++ toys/test.ml
@@ -25,18 +25,65 @@ let write log es =
   in
   List.iter do_one es
 
-let getk log k c = 
+let get log k = 
   let rec find_pos pos = 
     match get_entry log pos with
-      | NIL -> failwith "Not_found"
-      | V v -> c v
-      | L (k0,p0) -> if k = k0 then find_pos p0 else failwith "Not_found"
+      | V v -> v
+      | L (k0,p0) when k = k0 -> find_pos p0 
       | N (l,k0,r) -> let p' = if k <= k0 then l else r  in
 		      find_pos p'			
+      | _ -> failwith "Not_found"
   in
   find_pos (root_pos log)
-    
-let get log k = getk log k (fun v -> v)
+
+type dir = Hit | Left | Right
+type trail = (dir * entry * pos) list
+
+exception Todo of trail * string
+let todo v msg = raise (Todo (v,msg))
+
+let rec build_set k v start (visited:trail)  = V v :: do_start start k visited 
+and do_start start k visited = match visited with
+  | [] -> [L (k, start) ]
+  | (dir , L(k0,p0),pe) :: rest ->
+    begin
+      let e = L(k,start) in
+      match dir with
+	| Hit   -> e :: do_rest (start + 1) rest
+	| Left  -> e :: N(start + 1, k, pe) :: do_rest (start + 2) rest
+	| Right -> e :: N(pe, k0, start +1) :: do_rest (start + 2) rest
+    end
+  | _ -> todo visited (Printf.sprintf "do_start %i '%s'" start k)
+and do_rest start visited = 
+  match visited with
+    | [] -> []
+    | h :: t ->
+      let e = 
+	match h with
+	  | (Left , N(pl,k0,pr),pe)  ->  N(start,k0,pr) 
+	  | (Right, N(pl,k0,pr),pe)  ->  N(pl,k0,start)
+	  | _ -> todo visited (Printf.sprintf "do_rest %i" start)
+      in
+      e :: do_rest (start + 1) t
+
+let set log k v =
+  let rec descend trail pos = 
+    match get_entry log pos with
+      | NIL -> trail
+      | V v -> failwith "corrupt"
+      | L (k0,p0) as e -> let dir = 
+			    if k = k0 
+			    then Hit else if k < k0 
+			      then Left 
+			      else Right 
+			  in
+			  (dir , e, pos) :: trail
+      | N (l,k0,r) as e when k <= k0 -> descend ((Left,e, pos) :: trail) l
+      | N (l,k0,r) as e              -> descend ((Right,e,pos) :: trail) r
+  in
+  let trail = descend [] (root_pos log) in
+  let update = build_set k v log.next trail in
+  write log update
 
 let dump log = 
   Array.iteri (fun i e->
@@ -69,7 +116,20 @@ let dot_log ?(f = stdout) log =
     if e <> NIL && i > 0 then Printf.fprintf f "\tnode%i -> node%i [style = invis];\n" i (i-1)
   ) log.es in
   Printf.fprintf f "}"
-    
+
+let view ?(v=dot_log) log = 
+  let root = "test" in
+  let dot = Filename.temp_file root ".dot" in
+  let png = Filename.temp_file root ".png" in
+  let oc = open_out dot in
+  let () = v ~f:oc log in
+  close_out oc;
+  let convert_cmd = Printf.sprintf "dot -Tpng -o %s %s" png dot in
+  let _ = Sys.command convert_cmd in
+  let cmd = Printf.sprintf "evince %s" png in
+  Sys.command cmd
+
+let view_log  log = view ~v:dot_log log
 
 let dot_tree ?(f= stdout) log =
   Printf.fprintf f "digraph Tree{\n";
@@ -92,74 +152,27 @@ let dot_tree ?(f= stdout) log =
   walk (root_pos log);
   Printf.fprintf f "}\n"
 
-let view ?(v=dot_tree) log = 
-  let root = "test" in
-  let dot = Filename.temp_file root ".dot" in
-  let png = Filename.temp_file root ".png" in
-  let oc = open_out dot in
-  let () = v ~f:oc log in
-  close_out oc;
-  let convert_cmd = Printf.sprintf "dot -Tpng -o %s %s" png dot in
-  let _ = Sys.command convert_cmd in
-  let cmd = Printf.sprintf "evince %s" png in
-  Sys.command cmd
 
 let view_tree log = view ~v:dot_tree log
-let view_log  log = view ~v:dot_log log
-
-    
-    
-
-type dir = Hit | Left | Right
-type trail = (dir * entry * pos) list
-exception Todo of trail * string
-
-let todo v msg = raise (Todo (v,msg))
-
-
-let set log k v =
-  let rec build_set (visited:trail)  = V v :: do_start (log.next) k visited 
-  and do_start start k visited = match visited with
-    | [] -> [L (k, start) ]
-    | (Hit ,  L(k0,p0),pe) :: rest -> 
-      L(k, start) :: do_rest (start + 1 ) rest
-    | (Left,  L(k0,p0),pe) :: rest -> 
-      L(k, start) :: N (start + 1, k, pe)  :: do_rest (start + 2) rest
-    | (Right, L(k0,p0),pe) :: rest -> 
-      L(k, start) :: N (p0 , k0,start + 1) :: do_rest (start + 2) rest
-    | _ -> todo visited (Printf.sprintf "do_start %i '%s'" start k)
-  and do_rest start visited = 
-    match visited with
-      | [] -> []
-      | (Left, N(pl,k0,pr),pe) :: rest -> 
-	N (start, k0, pr) :: do_rest (start + 1) rest
-      | (Right, N(pl,k0,pr),pe) :: rest ->
-	N (pl,k0,start) :: do_rest (start + 1) rest
-      | _ -> todo visited (Printf.sprintf "do_rest %i" start)
-  in
-  let rec descend visited pos = 
-    match get_entry log pos with
-      | NIL -> visited
-      | V v -> failwith "corrupt"
-      | L (k0,p0) as e when k < k0   -> (Left ,e, pos) :: visited
-      | L (k0,p0) as e when k = k0   -> (Hit  ,e, pos) :: visited
-      | L (k0,p0) as e               -> (Right,e, pos) :: visited
-      | N (l,k0,r) as e when k <= k0 -> descend ((Left,e, pos):: visited) l
-      | N (l,k0,r) as e              -> descend ((Right,e,pos) :: visited) r
-  in
-  let visited = descend [] (root_pos log) in
-  let path = build_set visited in
-  write log path
 
 
 let t0 = make 30 ;;
-List.iter (fun (k,v) -> set t0 k v) 
+let inserts =  
   ["f","F";  
    "d","D";
    "h","H";
    "a","A";
    "z","Z";
   ];;
+List.iter (fun (k,v) -> set t0 k v) inserts;;
+
+let t1 = make 10;;
+let i1s = ["d","D"; "f","F";];;
+List.iter (fun (k,v) -> set t1 k v) i1s;;
+
+let check () = 
+  List.iter (fun (k,v) -> let v' = get t0 k in assert (v = v'))
+
 
 
 
