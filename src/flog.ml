e5b6dacadb7a85dbc83f3a5d4a5eff4de1e5ca00
Nicolas Trangez <ikke@nicolast.be>
2011-10-24 18:48:19 +0200

    Work on file-based log implementation
    Basic testcase now succeeds, but code needs major fixups in different places.
    Disk format isn't fixed yet, either.

--- src/flog.ml
+++ src/flog.ml
@@ -20,6 +20,7 @@
 open Unix
 
 open Entry
+open Posix
 
 type t = {
   fd_in: file_descr;
@@ -34,53 +35,11 @@ type slab = {
   mutable pos: int;
 }
 
-let lseek_set f o =
-  let o' = lseek f o SEEK_SET in
-  if o' <> o then failwith "Flog.seek_set: seek failed" else ()
-
-let create f b =
-  let fd = openfile f [O_WRONLY; O_EXCL; O_CREAT] 0o644
-  and meta_garbage = String.make b '.' in
-
-  set_close_on_exec fd;
-
-  let _ = write fd "." 0 1 in
-  lockf fd F_TLOCK 1;
-
-  lseek_set fd 0;
-  ftruncate fd (2 * b);
-  let _ = write fd meta_garbage 0 b in
-  let _ = write fd meta_garbage 0 b in
-
-  close fd
-
-let make (f: string): t =
-  let fd_random = openfile f [O_WRONLY;] 0o644 in
-  set_close_on_exec fd_random;
-
-  lseek_set fd_random 0;
-  lockf fd_random F_TLOCK 1;
-
-  (* TODO Make safe -> close handles! *)
-  (* TODO Read metadata *)
-  let fd_in = openfile f [O_RDONLY] 0o644 in
-  set_close_on_exec fd_in;
-  let fd_append = openfile f [O_APPEND; O_WRONLY] 0o644 in
-  set_close_on_exec fd_append;
-
-  let offset = lseek fd_append 0 SEEK_END in
-
-  { fd_in=fd_in; fd_append=fd_append; fd_random=fd_random; offset=offset;
-    root_offset=(2 * 4096) }
-
 let chr0 = Char.chr 0
 let chr1 = Char.chr 1
 let chr2 = Char.chr 2
 let chr3 = Char.chr 3
-
-let int8_placeholder = '0'
-let int32_placeholder = "0123"
-let int64_placeholder = "01234567"
+let chr4 = Char.chr 4
 
 let write_uint8 i s o =
   assert (i >= 0);
@@ -88,6 +47,9 @@ let write_uint8 i s o =
   assert (o <= String.length s - 1);
 
   String.set s o (Char.chr i)
+
+and read_uint8 s o =
+  Char.code (String.get s o)
 
 let write_uint32 i s o =
   assert (i >= 0);
@@ -143,6 +105,185 @@ let read_uint64 s o =
 
   i0 + (i1 lsl 8) + (i2 lsl 16) + (i3 lsl 24) + (i4 lsl 32) + (i5 lsl 40)
     + (i6 lsl 48) + (i7 lsl 56)
+
+let lseek_set f o =
+  let o' = lseek f o SEEK_SET in
+  if o' <> o then failwith "Flog.seek_set: seek failed" else ()
+
+let crc32 s o l = 0xDEADBEEF (* TODO *)
+
+let metadata_prefix = "BaArDsKeErDeR"
+let metadata_suffix = "bAaRdSkEeRdEr"
+let metadata_version = 0x01
+
+type blocksize = int
+type spindle = int
+type offset = int
+type count = int
+
+type metadata = {
+  md_blocksize: blocksize;
+  md_spindle: spindle;
+  md_offset: offset;
+  md_count: count;
+}
+
+let serialize_metadata md =
+  let s = String.make md.md_blocksize chr0
+  and pl = String.length metadata_prefix
+  and sl = String.length metadata_suffix in
+  String.blit metadata_prefix 0 s 0 pl;
+  write_uint8 metadata_version s pl;
+  write_uint32 md.md_blocksize s (pl + 1);
+  write_uint8 md.md_spindle s (pl + 5);
+  write_uint64 md.md_offset s (pl + 6);
+  write_uint32 md.md_count s (pl + 14);
+  String.blit metadata_suffix 0 s (pl + 18) sl;
+
+  let crc = crc32 s 0 (pl + 18 + sl) in
+  write_uint32 crc s (pl + 18 + sl);
+
+  (s, md.md_blocksize)
+
+and deserialize_metadata s =
+  let p1 = String.sub s 0 (String.length metadata_prefix) in
+  assert (p1 = metadata_prefix);
+
+  let pl = String.length p1
+  and sl = String.length metadata_suffix in
+
+  let v = read_uint8 s pl in
+  assert (v = metadata_version);
+
+  let bs = read_uint32 s (pl + 1) in
+
+  assert (String.length s >= bs);
+
+  let sp = read_uint8 s (pl + 5)
+  and o = read_uint64 s (pl + 6)
+  and c = read_uint32 s (pl + 14)
+  and p2 = String.sub s (pl + 18) sl in
+
+  assert (p2 = metadata_suffix);
+
+  let crc = read_uint32 s (pl + 18 + sl) in
+
+  if (crc32 s 0 (pl + 18 + sl) = crc)
+  then Some { md_blocksize=bs; md_spindle=sp; md_offset=o; md_count=c }
+  else None
+
+let create (f: string) (b: blocksize) =
+  let fd = openfile f [O_WRONLY; O_EXCL; O_CREAT] 0o644
+  and metadata1, b1 = serialize_metadata
+      { md_blocksize=b; md_spindle=0; md_offset=0; md_count=0 }
+  and metadata2, b2 = serialize_metadata
+      { md_blocksize=b; md_spindle=0; md_offset=0; md_count=1 } in
+
+  set_close_on_exec fd;
+
+  lockf fd F_TLOCK 0;
+
+  lseek_set fd 0;
+  ftruncate fd (2 * b);
+  let _ = write fd metadata1 0 b1 in
+  let _ = write fd metadata2 0 b2 in
+
+  close fd
+
+type commit = Commit of offset
+
+let serialize_commit (Commit o) =
+  let s = String.create (4 + 1 + 8 + 4) in
+
+  write_uint32 (1 + 8 + 4) s 0;
+  String.set s 8 chr4;
+  write_uint64 o s (4 + 1);
+  let crc = crc32 s (4 + 1 + 8) in
+  write_uint32 o s (4 + 1 + 8);
+
+  s
+
+and deserialize_commit s o =
+  Commit (read_uint64 s o)
+
+let find_root f o =
+  let s = String.create 4
+  and t = String.create 1 in
+
+  let rec loop a o =
+    try
+      pread_into_exactly f s 4 o;
+      pread_into_exactly f t 1 (o + 4);
+
+      let s' = read_uint32 s 0 in
+
+      match String.get t 0 with
+        | i when i = chr1 -> loop a (o + 4 + s')
+        | i when i = chr2 -> loop a (o + 4 + s')
+        | i when i = chr3 -> loop a (o + 4 + s')
+        | i when i = chr4 ->
+            let b = String.create (s' - 1) in
+            pread_into_exactly f b (o + 4 + 1) (o + 4 + 1);
+            let (Commit c) = deserialize_commit b 0 in
+            loop c (o + 4 + s')
+        | _ -> failwith "Flog.find_root: unknown entry type"
+
+    with End_of_file ->
+      a
+  in
+
+  loop 0 o
+
+let make (f: string): t =
+  let from_some = function
+    | Some x -> x
+    | _ -> invalid_arg "Flog.make.from_some"
+  in
+
+  let fd_random = openfile f [O_WRONLY;] 0o644 in
+  set_close_on_exec fd_random;
+
+  lseek_set fd_random 0;
+  lockf fd_random F_TLOCK 0;
+
+  (* TODO Make safe -> close handles! *)
+  (* TODO Read metadata *)
+  let fd_in = openfile f [O_RDONLY] 0o644 in
+  set_close_on_exec fd_in;
+
+  let size = 2 * 4096 in
+  let mds = String.create size in
+  pread_into_exactly fd_in mds size 0;
+  let md1 = from_some (deserialize_metadata mds) in
+  let bs = md1.md_blocksize in
+  let mds = String.create bs in
+  pread_into_exactly fd_in mds bs bs;
+  let md2 = from_some (deserialize_metadata mds) in
+
+  (* TODO Use 'best' metadata instead of checking it's value *)
+  assert (md1.md_blocksize == md2.md_blocksize);
+  assert (md1.md_spindle == md2.md_spindle);
+
+  let fd_append = openfile f [O_APPEND; O_WRONLY] 0o644 in
+  set_close_on_exec fd_append;
+
+  let offset = lseek fd_append 0 SEEK_END in
+
+  let root = find_root fd_in (2 * bs) in
+
+  { fd_in=fd_in; fd_append=fd_append; fd_random=fd_random; offset=offset;
+    root_offset=root }
+
+let close db =
+  Unix.close db.fd_in;
+  Unix.close db.fd_append;
+  Unix.close db.fd_random
+
+let int8_placeholder = '0'
+let int32_placeholder = "0123"
+let int64_placeholder = "01234567"
+
+let marker = 0x0baadeed
 
 let serialize_leaf l =
   let b = Buffer.create 256 in
@@ -294,25 +435,28 @@ let write t slab =
         let s = serialize_index i in
         do_write s
   in
-  List.iter write_slab (List.rev slab.entries)
+  List.iter write_slab (List.rev slab.entries);
+
+  let ro = t.root_offset in
+  let s = serialize_commit (Commit ro) in
+  let sl = String.length s in
+  let w = write t.fd_append s 0 sl in
+  assert (w = sl);
+  update_offset sl
 
 let root t = t.root_offset
 let next t = t.offset
 let read t pos =
+  if pos = 0 then NIL
+  else
   let s = String.create 4 in
 
-  (* TODO Use pread(2) *)
-  lseek_set t.fd_in pos;
-
-  let c = read t.fd_in s 0 4 in
-  assert (c = 4);
+  pread_into_exactly t.fd_in s 4 pos;
 
   let l = read_uint32 s 0 in
   let s = String.create l in
-  let c = read t.fd_in s 0 l in
-  assert (c = l);
+  pread_into_exactly t.fd_in s l (pos + 4);
 
-  Printf.printf "s = %s\n" s;
   match String.get s 0 with
     | i when i = chr1 -> deserialize_value s 1
     | i when i = chr2 -> deserialize_leaf s 1
---
+++ src/flog_test.ml
@@ -0,0 +1,171 @@
+(*
+ * This file is part of Baardskeerder.
+ *
+ * Copyright (C) 2011 Incubaid BVBA
+ *
+ * Baardskeerder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baardskeerder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baardskeerder.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+open OUnit
+open Unix
+
+open Tree
+open Flog
+
+let test_uintN wf rf l n () =
+  let rec loop = function
+    | -1 -> ()
+    | n ->
+        let l' = l + 10 in
+        let s = String.create l'
+        and o = Random.int (l' - l) in
+        wf n s o;
+        OUnit.assert_equal ~printer:string_of_int n (rf s o);
+        loop (pred n)
+  in
+  loop 0xFF
+
+let base_serialization =
+  "base_serialization" >::: [
+    "uint8" >:: test_uintN write_uint8 read_uint8 1 0xFF;
+    "uint32" >:: test_uintN write_uint32 read_uint32 4 0xFFFFFFFF;
+    "uint64" >:: test_uintN write_uint64 read_uint64 8 max_int;
+  ]
+
+let test_metadata_serialization () =
+  let from_some = function
+    | Some x -> x
+    | _ -> invalid_arg "test_metadata_serialization: from_some"
+  in
+
+  let b = 4096 in
+  let md1 = { md_blocksize=b; md_spindle=0; md_offset=0; md_count=0 } in
+  let md2 = { md1 with md_spindle=1; md_offset=654321; md_count=456789 } in
+
+  let s1, b1 = serialize_metadata md1
+  and s2, b2 = serialize_metadata md2 in
+
+  OUnit.assert_equal ~printer:string_of_int b b1;
+  OUnit.assert_equal ~printer:string_of_int b b2;
+
+  let md1' = from_some (deserialize_metadata s1)
+  and md2' = from_some (deserialize_metadata s2) in
+
+  OUnit.assert_equal md1 md1';
+  OUnit.assert_equal md2 md2'
+
+let metadata =
+  "metadata" >::: [
+    "serialization" >:: test_metadata_serialization;
+  ]
+
+let test_serialize_deserialize_entries () =
+  (* TODO This isn't a nice unit-test, since it tests multiple types/functions.
+   * Some QuickCheck-like mechanism generating nodes at random, then performing
+   * a comparison after serialization/deserialization might be useful.
+   *)
+  let kps = [("a", 1); ("b", 2); ("c", 0xFFFFFFFF); ("def", max_int)] in
+
+  let leaf = kps
+  and index = (54321, kps)
+  and value = "This is a test value" in
+
+  let sl = Flog.serialize_leaf leaf
+  and si = Flog.serialize_index index
+  and sv = Flog.serialize_value value in
+
+  let leaf' = Flog.deserialize_leaf sl 5
+  and index' = Flog.deserialize_index si 5
+  and value' = Flog.deserialize_value sv 5 in
+
+  OUnit.assert_equal ~printer:Entry.entry2s (Entry.Leaf leaf) leaf';
+  OUnit.assert_equal ~printer:Entry.entry2s (Entry.Index index) index';
+  OUnit.assert_equal ~printer:Entry.entry2s (Entry.Value value) value'
+
+let entries =
+  "entries" >::: [
+    "serialization" >:: test_serialize_deserialize_entries;
+  ]
+
+let with_tempfile f = fun () ->
+  let pid = Unix.getpid () in
+  let fn = Printf.sprintf "test_%d.db" pid in
+
+  let do_unlink () =
+    try
+      Unix.unlink fn
+    with
+      Unix.Unix_error (Unix.ENOENT, _, _) -> ()
+  in
+
+  try
+    f fn;
+    do_unlink ()
+  with e ->
+    do_unlink ();
+    raise e
+
+let test_database_create fn =
+  create fn 4096;
+  let s = Unix.stat fn in
+  OUnit.assert_equal (s.st_size = 2 * 4096)
+
+let test_database_make fn =
+  let () = create fn 4096 in
+  let db = make fn in
+  close db
+
+module MDB = DB(Flog)
+
+let with_database f =
+  let f' fn =
+    create fn 4096;
+    let db = make fn in
+
+    try
+      f db;
+      close db
+    with e ->
+      close db;
+      raise e
+  in
+  with_tempfile f'
+
+let test_database_set db =
+  MDB.set db "foo" "bar"
+
+let test_database_set_get db =
+  let k = "foo"
+  and v = "bar" in
+
+  MDB.set db k v;
+
+  let v' = MDB.get db k in
+
+  OUnit.assert_equal v v'
+
+let database =
+  "database" >::: [
+    "create" >:: with_tempfile test_database_create;
+    "make" >:: with_tempfile test_database_make;
+    "set" >:: with_database test_database_set;
+    "set_get" >:: with_database test_database_set_get;
+  ]
+
+let () =
+  let run s = let _ = run_test_tt_main s in () in
+  run base_serialization;
+  run metadata;
+  run entries;
+  run database
