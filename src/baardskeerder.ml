fdd0bcce2e02554845013815fd11e05b3be8c81b
Romain Slootmaekers <romain@incubaid.com>
2011-12-12 20:08:20 +0100

    wrestling with baardskeerder as an ocamlfind installable library

---
+++ src/baardskeerder.ml
@@ -0,0 +1,26 @@
+(*
+ * This file is part of Baardskeerder.
+ *
+ * Copyright (C) 2011 Incubaid BVBA
+ *
+ * Baardskeerder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baardskeerder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baardskeerder.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+module MyLog = Flog0
+
+type t = Flog0.t
+
+let make fn = MyLog.make fn 
+
+let close log = MyLog.close log
