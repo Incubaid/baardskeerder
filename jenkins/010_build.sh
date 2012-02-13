6ac48d23409a97d4d584493ec225f6d1346d37a4
Nicolas Trangez <ikke@nicolast.be>
2012-02-13 15:15:52 +0100

    CI: Build and install ocaml-quickcheck
    Change-Id: I08cd22565a16ac1b001fed50d93d682d5f8ffaef

--- jenkins/010_build.sh
+++ jenkins/010_build.sh
@@ -2,6 +2,7 @@
 
 export PATH=/home/qbase/ROOT/OCAML/bin:$PATH
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:/home/qbase/ROOT/OCAML/lib
+export OCAMLPATH=_deps/inst
 which ocamlbuild
 ocaml -version
 cd src && ocamlbuild -use-ocamlfind bsmgr.byte bsmgr.native
